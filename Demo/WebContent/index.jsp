d0f4708261a27e0fd0e09fabd4d7325dfaa7371f
lbk2kgit <71619375+bin98123@users.noreply.github.com>
2021-05-31 22:50:02 +0700

    login page

---
+++ Demo/WebContent/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+<script type="text/javascript">
+	window.location.href = "./login.jsp"
+</script>
+</head>
+<body>
+
+</body>
+</html>
---
+++ Demo/src/servlet/LoginServlet.java
@@ -0,0 +1,66 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.User;
+import dao.UserDAO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private User user;
+	private UserDAO userDAO;
+	private static final long serialVersionUID = 1L;
+	private String connectionUrl = "jdbc:sqlserver://localhost:1433;" + "databaseName=Shop;user=sa;password=root";
+	private String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		boolean ok = false;
+		String userName = request.getParameter("userName");
+		String userPassword = request.getParameter("Password");
+		user = new User();
+		userDAO = new UserDAO();
+		userDAO.setUserName(userName);
+		userDAO.setUserPassword(userPassword);
+
+		if (userName != null && userPassword != null || userName != "" && userPassword != "") {
+			ok = true;
+		}
+		try {
+			if (ok == true) {
+				boolean login = user.getLogin(userDAO.getUserName(), userDAO.getUserPassword());
+
+				if (login == true) {
+					request.getRequestDispatcher("/WelcomeUser.jsp").forward(request, response);
+				} else {
+					// username: 'Khanh'
+					//password: '12345'
+//					String userDetails = "Invalid Username or Password. Username and Password are case-sensitive.";
+					String userDetails = "Invalid Username or Password.";
+					request.setAttribute("Invalid", userDetails);
+					request.getRequestDispatcher("/login.jsp").forward(request, response);
+
+				}
+			} else {
+				request.getRequestDispatcher("/index.jsp").forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+	}
+}
