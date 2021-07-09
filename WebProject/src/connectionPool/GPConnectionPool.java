ecdc6eaa845e7c32602845f5cb15bef473f09737
damanh1905 <18130005@st.hcmuaf.edu.vn>
2021-07-10 00:13:41 +0700

    updateproject

--- WebProject/src/connectionPool/GPConnectionPool.java
+++
@@ -1,9 +0,0 @@
-package connectionPool;
-
-import java.sql.Connection;
-
-public interface GPConnectionPool {
-	Connection getConnection();
-	 
-    boolean releaseConnection(Connection connection);
-}
